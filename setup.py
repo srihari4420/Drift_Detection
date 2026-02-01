7f7cffed42d609cc46a3186d79fe8a5b6fb7fa4b
srihari4420 <sriharichowdary4420.com>
2026-02-01 18:52:47 +0000

    Inserted csv to mongodb

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Drift_Detection",
+    version = "0.1",    
+    author="srihari",
+    author_email="sriharichowdary4420@gmail.com",
+    packages=find_packages(),
+)
